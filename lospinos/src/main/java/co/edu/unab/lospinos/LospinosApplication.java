961f84633be095d2a34684180bac26b517e9bc3c
Paola <queypaola@hotmail.com>
2022-10-22 11:57:34 -0500

    Creacion Backend

---
+++ lospinos/src/main/java/co/edu/unab/lospinos/LospinosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unab.lospinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LospinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LospinosApplication.class, args);
+	}
+
+}
